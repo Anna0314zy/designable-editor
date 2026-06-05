da299d4608bb7869ef47790bf8eda73908e3c961
zouyu01 <yu.zou@sparkedu.com>
2026-06-05 08:25:30 +0800

    feat: 改成http

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 import packageJson from './package.json';
 import { resolve } from 'path';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -64,7 +64,7 @@ export default defineConfig({
       },
     }),
     viteCommonjs({}),
-    basicSsl(),
+    // basicSsl(),
     react()
   ]
 })
